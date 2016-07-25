a3dfff6dd65ad24a7239306621cf783f233097c8
bklynate <nathanielcfa@gmail.com>
2016-07-25 09:56:10 -0400

    add routes for login and signup

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ app.get("/", function(req, res){
   res.render("index");
 });
 
+app.get("/login", function(req,res){
+  res.send("Hello from login");
+});
+
+app.get("/signup", function(req,res){
+  res.send("Hello from signup");
+});
+
 app.listen(app.get('port'), function(){
   console.log("Harambee!! Harambee!! Harambee!! Harambee!!");
 });
