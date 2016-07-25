bfac6a20aab80c9e834ca65b34dcbfccb17451fd
bklynate <nathanielcfa@gmail.com>
2016-07-25 08:55:48 -0400

    fix error

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ var express = require('express');
 var app = express();
 
 app.set("view engine", "ejs");
+app.set('port', (process.env.PORT || 3000));
 
 app.get("/", function(req, res){
   res.send("<h1>Hello from Harambee!</h1>")
 });
 
-app.listen(3000, function(){
+app.listen(app.get('port'), function(){
   console.log("Harambee!! Harambee!! Harambee!! Harambee!!");
 });
