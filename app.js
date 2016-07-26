c0f0a700301de99ac8db3c50fc2d29190a9421c6
bklynate <nathanielcfa@gmail.com>
2016-07-26 08:05:35 -0400

    add css tweaks

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.set('port', (process.env.PORT || 3000));
-app.use(express.static("public"));
 
 app.get("/", function(req, res){
   res.render("index");
