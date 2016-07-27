162e8541986c41e96ef954dcce0e8af07aa30954
bklynate <nathanielcfa@gmail.com>
2016-07-27 11:59:10 -0400

    edit model

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
 var express = require('express');
 var app = express();
 var User = require("./models/user");
+var passport = require("passport");
+var LocalStrategy = require("passport-local");
+var passportLocalMongoose = require("passport-local-mongoose");
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.set('port', (process.env.PORT || 3000));
+
+//=============================
+//=====PASSPORT CONFIG=========
+//=============================
+app.use(require("express-session")({
+  secret: "394583059834950",
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+// THEY READ THE SESSION ENCODES AND
+// THEN DECODES THE SESSION
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+//=============================
+//=====      END      =========
+//=============================
 
 app.get("/", function(req, res){
   res.render("index");
--- models/user.js
+++ models/user.js
@@ -1,1 +1,15 @@
 var mongoose = require('mongoose');
+var passportLocalMongoose = require('passport-local-mongoose');
+
+var userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+  created:{
+    type: Date,
+    default: Date.now
+  }
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model("User", userSchema);
